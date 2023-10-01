ee9ca3f965eb76238fc972185daace36b45efb13
Christian Hoffmann <christian.hoffmann@sigmatechnology.com>
2023-10-01 12:52:27 +0200

    implemented basic enemy spawner

---
+++ Vampire Survivors Clone/Helpers/spawn_info.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name SpawnInfo
+
+@export var time_start: int = 0
+@export var time_end: int = 0
+@export var enemy: Resource
+@export var enemy_num: int
+@export var enemy_spawn_delay: int
+
+var spawn_delay_counter = 0
