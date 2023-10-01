9ab013b668d109c6c8066764984c1355915cfd02
Christian Hoffmann <christian.hoffmann@sigmatechnology.com>
2023-10-01 13:30:41 +0200

    added defaults to spawner, added second wave

--- Vampire Survivors Clone/Helpers/spawn_info.gd
+++ Vampire Survivors Clone/Helpers/spawn_info.gd
@@ -5,7 +5,7 @@ class_name SpawnInfo
 @export var time_start: int = 0
 @export var time_end: int = 0
 @export var enemy: Resource
-@export var enemy_num: int
-@export var enemy_spawn_delay: int
+@export var enemy_num: int = 1
+@export var enemy_spawn_delay: int = 1
 
 var spawn_delay_counter = 0
